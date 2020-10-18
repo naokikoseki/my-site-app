a21c105c2c3afd3aebb0d88d61845bd9bdd6fbbb
naokikoseki <naoki.koseki0220@gmail.com>
2020-10-18 22:57:29 +0900

    微調整

--- app/javascript/background_image.js
+++ app/javascript/background_image.js
@@ -2,7 +2,7 @@ $(function() {
   let i = 1;
   setInterval(function() {
     if(i > 6) i = 1;
-    $("#background").css({ "background-image": "asset-url('sample"+i+".jpg')"});
+    $("#background").css({ "background-image": "url('assets/sample"+i+".jpg')"});
     i++;
     }, 2500
   );
